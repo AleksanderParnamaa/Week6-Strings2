9de91d37ed628c2d7c5d2983309ac9ed3d9c17e0
Opilane TTHK <opilane@tthk.ee>
2021-10-06 11:28:32 +0300

    first commit

---
+++ DontPanic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DontPanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = ("Dont Panic").ToUpper();
+            for (int i = name.Length - 1; i > -1; i--)
+            {
+                Console.WriteLine(name[i]);
+                
+            }
+        }
+    }
+}
---
+++ DontPanicNCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DontPanicNCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Counter = 0;
+            string dont = ("dont panic");
+            for (int i = 0; i < dont.Length; i++)
+            {
+                if (dont[i] == 'n')
+                {
+                    Counter++; 
+                }
+
+            }
+            if (Counter == 1)
+            {
+                Console.WriteLine($"The sentence dont panic has {Counter} letters 'n' in it");
+            }
+            else
+            {
+                Console.WriteLine($"The sentence dont panic has {Counter} letters 'n' in it");
+            }
+        }
+    }
+}
